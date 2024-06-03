087e36135e55f90bd1f1b58b26ccb87fb983e459
Sage Welky <ehrenwelky@gmail.com>
2024-06-03 14:10:19 -0500

    Hooked up the button and score logic, edited the styling of all visible elements, hooked up the form resets, changed the factory function logic to use the "this" keyword to properly access the scope where the name and color player properties are. Made some minor edits to the html such as adding id tags for the control container buttons, and cleaned up various bugs such as calling the name function when trying to update player color from the game instance function.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let gameEnvironment = (function() {
 
   let gameBoard = (function() {
+
     let boardState = [["c","c","c"],
                       ["c","c","c"],
                       ["c","c","c"]];
@@ -9,8 +10,6 @@ let gameEnvironment = (function() {
       boardState = [["c","c","c"],
                     ["c","c","c"],
                     ["c","c","c"]];
-      player1.resetScore();
-      player2.resetScore();
     }
     function getBoardState() {
       return boardState;
@@ -19,22 +18,86 @@ let gameEnvironment = (function() {
       boardState[columnIndex][rowIndex] = value;
     }
     return {resetBoard, getBoardState, setBoardState};
+
   })();
 
   let gameDisplayContainer = document.querySelector("#game-display-container");
+  let controlButtons = document.querySelectorAll(".control-button");
+  let editPlayerDialog = document.getElementById("edit-player-dialog");
+  let editPlayerForm = document.getElementById("edit-player-form");
+  let cancelDialog = document.getElementById("cancel-dialog");
+  let scorePlayer1 = document.getElementById("score-player1");
+  let namePlayer1 = document.getElementById("name-player1");
+  let scorePlayer2 = document.getElementById("score-player2");
+  let namePlayer2 = document.getElementById("name-player2");
+  let player1 = createPlayer("Player One", "red");
+  namePlayer1.style.color = `${player1.color}`;
+  let player2 = createPlayer("Player Two", "blue");
+  namePlayer2.style.color = `${player2.color}`;
+  let turn = 1;
+  let gameEndedState = false;
+  let editingPlayer;
 
   gameDisplayContainer.addEventListener( "click", (event) => {
-    console.log(event.target.textContent);
+
     if ( event.target.className === "move-space" && event.target.textContent === '\u00A0') {
       playRound(event);
     }
+
+  });
+  controlButtons.forEach( (controlButton) => {
+
+    if (controlButton.id === "reset-button") {
+      controlButton.addEventListener( "click", (event) => {
+        resetGame(true);
+      })
+    } else {
+      controlButton.addEventListener( "click", (event) => {
+        editingPlayer = event.target.id.slice(-1) === "1" ? "player1" : "player2";
+        editPlayerDialog.showModal();
+      })
+    }
+
+  })
+  editPlayerDialog.addEventListener( "submit", () => {
+
+    let playerName = document.querySelector("#player-name");
+    let playerColor = document.querySelector("#player-color");
+    if (playerName.value.trim() !== '') {
+      if (editingPlayer === "player1") {
+      changePlayerName(player1, playerName.value);
+      namePlayer1.textContent = player1.name + ":";
+      console.log(player1.name);
+      }
+      if (editingPlayer === "player2") {
+        changePlayerName(player2, playerName.value);
+        namePlayer2.textContent = player2.name + ":";
+        console.log(player2.name);
+        }
+    }
+    if (playerColor.value.trim() !== '') {
+      if (editingPlayer === "player1") {
+        console.log(playerColor.value);
+        changePlayerColor(player1, playerColor.value);
+        namePlayer1.style.color = `${player1.color}`;
+        console.log(player1.color);
+        }
+        if (editingPlayer === "player2") {
+          changePlayerColor(player2, playerColor.value);
+          namePlayer2.style.color = `${player2.color}`;
+          console.log(player2.color);
+        }
+    }
+    editPlayerForm.reset();
+
+  });
+  cancelDialog.addEventListener( "click", () => {
+
+    editPlayerDialog.close();
+
   });
 
-  let player1 = createPlayer("Player One", "red");
-  let player2 = createPlayer("Player Two", "blue");
-  let turn = 1;
-  console.log(turn);
-  let gameEndedState = false;
+
 
 
   function createPlayer(inputName, inputColor) {
@@ -52,13 +115,13 @@ let gameEnvironment = (function() {
       score = 0;
     };
     function setNewColor(newColor) {
-      color = newColor;
+      this.color = newColor;
     };
     function setNewName(newName) {
-      name = newName;
+      this.name = newName;
     };
 
-    return {name, color, increaseScore, getScore, resetScore, setNewColor, setNewName};
+    return {name: this.name, color, increaseScore, getScore, resetScore, setNewColor, setNewName};
   }
 
 
@@ -66,7 +129,7 @@ let gameEnvironment = (function() {
     playerToChange.setNewName(playerToChangeNewName);
   }
   function changePlayerColor(playerToChange, playerToChangeNewColor) {
-    playerToChange.setNewName(playerToChangeNewColor);
+    playerToChange.setNewColor(playerToChangeNewColor);
   }
   function playRound(event) {
 
@@ -78,37 +141,43 @@ let gameEnvironment = (function() {
 
     gameBoard.setBoardState(moveSpace.id.charAt(0), moveSpace.id.charAt(moveSpace.id.length-1), moveValue);
 
-    if (turn === 9) {
+    let board = gameBoard.getBoardState();
+    if (
+         (board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] !== "c")
+      || (board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] !== "c")
+      || (board[2][0] === board[2][1] && board[2][0] === board[2][2] && board[2][0] !== "c")
+      || (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] !== "c")
+      || (board[0][1] === board[1][1] && board[0][1] === board[2][1] && board[0][1] !== "c")
+      || (board[0][2] === board[1][2] && board[0][2] === board[2][2] && board[0][2] !== "c")
+      || (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] !== "c")
+      || (board[2][0] === board[1][1] && board[2][0] === board[0][2] && board[2][0] !== "c")
+    ) {
+      gameState = "Game Over!";
+      winningPlayer = moveValue === "X" ? player1 : player2;
+      gameEndedState = true;
+    } else if (turn === 9) {
       gameState = "Tie!";
       gameEndedState = true;
-    } else {
-      let board = gameBoard.getBoardState();
-      if (
-           (board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] !== "c")
-        || (board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] !== "c")
-        || (board[2][0] === board[2][1] && board[2][0] === board[2][2] && board[2][0] !== "c")
-        || (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] !== "c")
-        || (board[0][1] === board[1][1] && board[0][1] === board[2][1] && board[0][1] !== "c")
-        || (board[0][2] === board[1][2] && board[0][2] === board[2][2] && board[0][2] !== "c")
-        || (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] !== "c")
-        || (board[2][0] === board[1][1] && board[2][0] === board[0][2] && board[2][0] !== "c")
-      ) {
-        gameState = "Game Over!";
-        winningPlayer = moveValue === "X" ? player1 : player2;
-        gameEndedState = true;
-      }
     }
     turn++
     renderGame(moveValue, gameState, winningPlayer, moveSpace);
-    console.log(turn);
   }
 
   function renderGame(moveValue, gameState, winningPlayer, moveSpace) {
-    moveSpace.textContent = moveValue;
+    if (moveSpace) {
+      moveSpace.textContent = moveValue;
+    }
 
     if ( gameEndedState ) {
       let winningPlayerName = winningPlayer.name ? winningPlayer.name : winningPlayer;
-      setTimeout(() => {resolveGame(gameState, winningPlayerName)}, 100);
+      setTimeout(() => {
+        resolveGame(gameState, winningPlayerName);
+        scorePlayer1.textContent = player1.getScore();
+        scorePlayer2.textContent = player2.getScore();
+      }, 300);
+    } else {
+      scorePlayer1.textContent = player1.getScore();
+      scorePlayer2.textContent = player2.getScore();
     }
   }
 
@@ -116,10 +185,14 @@ let gameEnvironment = (function() {
     //.textContent = `${endState} \n ${winnerName} wins!`
     //.showModal()
     //event listener on modal will call resetGame()
-    alert(`${endState} \n ${winningPlayerName} wins!`);
+
+    console.log(winningPlayerName);
+    console.log(player1.name);
+    console.log(winningPlayerName === player1.name);
     switch (winningPlayerName) {
       case `${player1.name}`:
         player1.increaseScore();
+        console.log(player1.getScore());
         break;
       case `${player2.name}`:
         player2.increaseScore();
@@ -127,14 +200,22 @@ let gameEnvironment = (function() {
       default:
         break;
     }
-    resetGame();
+    alert(`${endState} \n ${winningPlayerName} wins!`);
+    resetGame(false);
   }
-  function resetGame() {
+
+  function resetGame(fullResetBool) {
     gameBoard.resetBoard();
     gameEndedState = false;
     let displayBoard = document.querySelectorAll(".move-space");
     displayBoard.forEach ((display) => {display.textContent = '\u00A0'}, displayBoard)
     turn = 1;
+
+    if(fullResetBool) {
+      player1.resetScore();
+      player2.resetScore();
+      renderGame();
+    }
   }
 
 
